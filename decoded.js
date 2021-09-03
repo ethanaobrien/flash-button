de8c4d4d4cabe5cd2ad71de1c48ff61609cc9324
Ethan O'Brien <ethan.a.obrien@gmail.com>
2021-09-03 14:24:44 -0500

    Working

--- decoded.js
+++ decoded.js
@@ -3,7 +3,8 @@
 		document.body.removeChild(document.body.firstChild);
 	};
     try {
-        var a = await fetch('https://rawcdn.githack.com/ethanaobrien/flash-button/main/ruffle.js');
+        window.paths = {wasm: 'https://raw.githack.com/ethanaobrien/flash-button/main/d0109ad57d5486eb7e0e.wasm'};
+        var a = await fetch('https://raw.githack.com/ethanaobrien/flash-button/main/ruffle.js');
         var blob = await a.blob();
         var url = URL.createObjectURL(blob);
         var b = document.createElement('script');
@@ -11,15 +12,17 @@
         document.body.appendChild(b);
     } catch(e) {
         alert(e.message);
+        return;
     };
     var file = document.createElement('input');
     file.type = 'file';
+    file.accept = '.swf,application/x-shockwave-flash';
     file.onchange = async function(e) {
         var Je = window.RufflePlayer.newest();
         var Ye = Je.createPlayer();
         Ye.id="player";
         document.body.appendChild(Ye);
-        Ye.load({data: await new Response(e.target.files[0]).arrayBuffer()});
+        Ye.load({data: await new Response(e.target.files[0]).arrayBuffer(), letterbox:"on", logLevel:"warn"});
     };
     file.click();
 })();
